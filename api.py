2dcb33cd2e88d984aca8c3c7c789e23e08e5929b
Aditya <38887904+adi0808@users.noreply.github.com>
2021-03-02 13:31:59 +0530

    Update api.py
    Final Commit

--- api.py
+++ api.py
@@ -1,1 +1,19 @@
+from flask import Flask
+import config
 
+app = Flask(__name__)
+
+
+# APi to return the stored message in memory and than flush them out
+@app.route('/api/<string:topic>', methods=['GET'])
+def get_data(topic):
+    if config.data_received[topic]:
+        data = config.data_received[topic]
+        config.data_received[topic] = []
+        return {"message": data}, 200
+    else:
+        return {"message": "Data not found"}, 404
+
+
+if __name__=="__main__":
+    app.run()
